27337045657314244cabb218c0a0932ab0063d3c
Durgaprasad549 <sreeramadurgaprasad@gmail.com>
2025-05-21 11:10:49 +0530

    Update Program-3.py

---
+++ Program-3.py
@@ -0,0 +1,27 @@
+''' Problem-3 : Get the total count of number listed in the dictionary which is multiple of [1,2,3,4,5,6,7,8,9]
+
+(examples)
+
+input: [1,2,8,9,12,46,76,82,15,20,30]
+
+Output:
+
+{1: 11, 2: 8, 3: 4, 4: 4, 5:3, 6: 2, 7:0, 8: 1, 9: 1}'''
+
+
+def count_multiple(numbers):
+    # Initialize a dictionary to hold the counts for multiples of 1 to 9
+    multiples_count = {i: 0 for i in range(1, 10)}
+    
+    # Count multiple for each number from 1 to 9
+    for num in numbers:
+        for i in range(1, 10):
+            if num % i == 0:
+                multiples_count[i] += 1
+                
+    return multiples_count
+
+if __name__ == "__main__":
+    input_numbers = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
+    result = count_multiple(input_numbers)
+    print(result)
